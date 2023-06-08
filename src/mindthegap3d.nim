c6f187154377ed2784ea1a94abc21d20a3756e5a
Jason Beetham <beefers331@gmail.com>
2023-06-07 23:56:23 -0600

    Update truss for diagnostics, and load font relative to app directory

--- src/mindthegap3d.nim
+++ src/mindthegap3d.nim
@@ -274,7 +274,7 @@ modelData.appendUv [vec2(0, 1), vec2(0, 0), vec2(1, 0), vec2(1, 1)].items
 
 
 proc gameInit() =
-  fontPath = "assets/fonts/MarradaRegular-Yj0O.ttf"
+  fontPath = getAppDir() / "assets/fonts/MarradaRegular-Yj0O.ttf"
   audio.init()
   invokeResourceProcs()
 
